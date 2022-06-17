c1c844c33f76040f5b4c4fafc04c6bd26484b5d6
Roman Ryabinin <roman.ryabinin@tgtdiagnostics.com>
2022-06-17 15:43:09 +0300

    создал тест на создание контакта

---
+++ addressbook-web-tests/addressbook-web-tests/Contact.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class Contact
+    {
+        string firstName;
+        string middleName;
+        string lastName;
+        string nickName;
+        string title;
+        string company;
+        string address;
+        string home;
+        string mobile;
+        string work;
+        string fax;
+        string email, email2, email3;
+        string homePage;
+        
+         
+
+        public Contact(string firstName, string middleName, string lastName)
+        {
+            this.firstName = firstName;
+            this.middleName = middleName;
+            this.lastName = lastName;
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string MiddleName
+        {
+            get { return middleName; }
+            set { middleName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public string NickName
+        {
+            get { return nickName; }
+            set { nickName = value; }
+        }
+        public String Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public string Company
+        {
+            get { return company; }
+            set
+            {
+                company = value;
+            }
+        }
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public string Home
+        {
+            get { return home; }
+            set { home = value; }
+        }
+
+        public string Mobile
+        {
+            get { return mobile; }
+            set { mobile = value; }
+        }
+
+        public string Work
+        {
+            get { return work; }
+            set { work = value; }
+        }
+
+        public string Fax
+        {
+            get { return fax; }
+            set { fax = value; }
+        }
+
+        public string Email
+        { 
+            get { return email; }
+            set { email = value; }
+        }
+
+        public string Email2
+        {
+            get { return email2; }
+            set { email2 = value; }
+        }
+
+        public string Email3
+        {
+            get { return email3; }
+            set { email3 = value; }
+        }
+
+        public string HomePage
+        {
+            get { return homePage; }
+            set { homePage = value; }
+        }
+    }
+}
--- addressbook-web-tests/addressbook-web-tests/GroupCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/GroupCreationTests.cs
@@ -16,6 +16,7 @@ namespace WebAddressbookTests
         private StringBuilder verificationErrors;
         private string baseURL;
         private bool acceptNextAlert = true;
+        private AccountData admin;
 
         [SetUp]
         public void SetupTest()
@@ -23,6 +24,7 @@ namespace WebAddressbookTests
             driver = new FirefoxDriver();
             baseURL = "https://localhost";
             verificationErrors = new StringBuilder();
+            admin = new AccountData("admin", "secret");
         }
 
         [TearDown]
@@ -43,7 +45,7 @@ namespace WebAddressbookTests
         public void GroupCreationTest()
         {
             OpenHomePage();
-            Login(new AccountData("admin", "secret"));
+            Login(admin);
             GoToGroupsPage();
             InitGroupCreation();
             GroupData group = new GroupData("a")
@@ -54,11 +56,119 @@ namespace WebAddressbookTests
             FillGroupForm(group);
             SubmitGroupCreation();
             ReturnToGroupsPage();
+            LogOut();
+        }
+
+        [Test]
+        public void ContactCreationTest()
+        {
+            OpenHomePage();
+            Login(admin);
+            GoToContactCreationPage();
+
+            Contact me = new Contact("roman", "v", "r")
+            {
+                NickName = "spectator",
+                Title = "king",
+                Company = "c",
+                Address = "x",
+                Home = "f",
+                Mobile = "d",
+                Work = "s",
+                Fax = "a",
+                Email = "q",
+                Email2 = "w",
+                Email3 = "e",
+                HomePage = "r"
+            };
+
+            FillContactForm(me);
+            CreateFilledContact();
+
+            LogOut();
+        }
+
+        private void CreateFilledContact()
+        {
+            driver.FindElement(By.XPath("//div[@id='content']/form/input[21]")).Click();
+        }
+
+        private void FillContactForm(Contact contact)
+        {
+            driver.FindElement(By.Name("firstname")).Click();
+            driver.FindElement(By.Name("firstname")).Clear();
+            driver.FindElement(By.Name("firstname")).SendKeys(contact.FirstName);
+
+            driver.FindElement(By.Name("middlename")).Click();
+            driver.FindElement(By.Name("middlename")).Clear();
+            driver.FindElement(By.Name("middlename")).SendKeys(contact.MiddleName);
+
+            driver.FindElement(By.Name("lastname")).Click();
+            driver.FindElement(By.Name("lastname")).Clear();
+            driver.FindElement(By.Name("lastname")).SendKeys(contact.LastName);
+
+            driver.FindElement(By.Name("nickname")).Click();
+            driver.FindElement(By.Name("nickname")).Clear();
+            driver.FindElement(By.Name("nickname")).SendKeys(contact.NickName);
+
+            driver.FindElement(By.Name("title")).Click();
+            driver.FindElement(By.Name("title")).Clear();
+            driver.FindElement(By.Name("title")).SendKeys(contact.Title);
+
+            driver.FindElement(By.Name("company")).Click();
+            driver.FindElement(By.Name("company")).Clear();
+            driver.FindElement(By.Name("company")).SendKeys(contact.Company);
+
+            driver.FindElement(By.Name("address")).Click();
+            driver.FindElement(By.Name("address")).Clear();
+            driver.FindElement(By.Name("address")).SendKeys(contact.Address);
+
+            driver.FindElement(By.Name("home")).Click();
+            driver.FindElement(By.Name("home")).Clear();
+            driver.FindElement(By.Name("home")).SendKeys(contact.Home);
+
+            driver.FindElement(By.Name("mobile")).Click();
+            driver.FindElement(By.Name("mobile")).Clear();
+            driver.FindElement(By.Name("mobile")).SendKeys(contact.Mobile);
+
+            driver.FindElement(By.Name("work")).Click();
+            driver.FindElement(By.Name("work")).Clear();
+            driver.FindElement(By.Name("work")).SendKeys(contact.Work);
+
+            driver.FindElement(By.Name("fax")).Click();
+            driver.FindElement(By.Name("fax")).Clear();
+            driver.FindElement(By.Name("fax")).SendKeys(contact.Fax);
+
+            driver.FindElement(By.Name("email")).Click();
+            driver.FindElement(By.Name("email")).Clear();
+            driver.FindElement(By.Name("email")).SendKeys(contact.Email);
+
+            driver.FindElement(By.Name("email2")).Click();
+            driver.FindElement(By.Name("email2")).Clear();
+            driver.FindElement(By.Name("email2")).SendKeys(contact.Email2);
+
+            driver.FindElement(By.Name("email3")).Click();
+            driver.FindElement(By.Name("email3")).Clear();
+            driver.FindElement(By.Name("email3")).SendKeys(contact.Email3);
+
+            driver.FindElement(By.Name("homepage")).Click();
+            driver.FindElement(By.Name("homepage")).Clear();
+            driver.FindElement(By.Name("homepage")).SendKeys(contact.HomePage);
+        }
+
+        private void GoToContactCreationPage()
+        {
+            driver.FindElement(By.LinkText("add new")).Click();
         }
 
         private void ReturnToGroupsPage()
         {
             driver.FindElement(By.LinkText("group page")).Click();
+            
+        }
+
+        private void LogOut()
+        {
             driver.FindElement(By.LinkText("Logout")).Click();
         }
 
@@ -72,9 +182,11 @@ namespace WebAddressbookTests
             driver.FindElement(By.Name("group_name")).Click();
             driver.FindElement(By.Name("group_name")).Clear();
             driver.FindElement(By.Name("group_name")).SendKeys(group.Name);
+
             driver.FindElement(By.Name("group_header")).Click();
             driver.FindElement(By.Name("group_header")).Clear();
             driver.FindElement(By.Name("group_header")).SendKeys(group.Header);
+
             driver.FindElement(By.Name("group_footer")).Click();
             driver.FindElement(By.Name("group_footer")).Clear();
             driver.FindElement(By.Name("group_footer")).SendKeys(group.Footer);
